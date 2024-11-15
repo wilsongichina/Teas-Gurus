62b426aafa4f3e880282a9a7889c7433ef866bc8
rupeshmandal7070 <mandal707036@gamil.com>
2024-11-15 15:25:40 +0530

    changes has done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-57BSRCCK' // Replace with your actual GTM ID
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
